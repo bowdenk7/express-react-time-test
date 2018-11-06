fd88a7b5cd0f855e6bb0495467020b30a9fb81fe
Bowden <wilkelly@microsoft.com>
2018-11-06 15:34:12 -0800

    more

--- client/src/Hero.js
+++ client/src/Hero.js
@@ -7,7 +7,7 @@ export default class Hero extends Component {
     return (
       <div id="hero">
         <img src={nodeLogo} alt="Azure App Service" />
-        <h1>Welcome to Bowden's House!</h1>
+        <h1>Welcome to Bowden's house!!!!!!!!!</h1>
         <p>This is a simple Node application using the popular <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> framework!</p>
       </div>
     );
